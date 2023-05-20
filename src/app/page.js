83598746d0d490eba8038353686aab201410f6a3
cjohnson <collin.g.johnson@gmail.com>
2023-05-20 16:58:44 -0400

    Add call to action text with social media tag

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,5 @@
 import Image from "next/image";
 
-const LinkedInLogoSvg = () => (
-  <a alt="Collin Johnson on LinkedIn" target="_blank" href="https://www.linkedin.com/in/collj/">
-    <Image src="socials/logo-linkedin.svg" width="24" height="24" alt="LinkedIn logo" />
-  </a>
-);
-
-const GithubLogoSvg = () => (
-  <a alt="Collin Johnson on Github" target="_blank" href="https://www.github.com/cjohnson/">
-    <Image src="socials/logo-github.svg" width="24" height="24" alt="Github logo" />
-  </a>
-);
-
 function HomeHeading() {
   return (
     <>
@@ -24,9 +12,19 @@ function HomeHeading() {
             <p className="max-w-2xl mb-6 font-medium text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
               Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan. He is working an internship at AdvantageCS.
             </p>
-            <div className="flex flex-row space-x-2 items-start">
-              <LinkedInLogoSvg />
-              <GithubLogoSvg />
+            <div className="flex flex-col space-y-3 items-start">
+              <a target="_blank" href="https://linkedin.com/in/collj/">
+                <div className="flex flex-row space-x-2">
+                  <Image src="socials/logo-linkedin.svg" width="24" height="24" alt="LinkedIn logo" />
+                  <p className="text-md">Connect on LinkedIn</p>
+                </div>
+              </a>
+              <a target="_blank" href="https://github.com/cjohnson/">
+                <div className="flex flex-row space-x-2">
+                  <Image src="socials/logo-github.svg" width="24" height="24" alt="Github logo" />
+                  <p className="text-md">Follow on Github</p>
+                </div>
+              </a>
             </div>
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
