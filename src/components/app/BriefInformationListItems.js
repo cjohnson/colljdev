2f2e9d5420ff66fe2c36689d66b60291c714221b
cjohnson <collin.g.johnson@gmail.com>
2023-05-21 15:01:01 -0400

    Add required keys to listed items

--- src/components/app/BriefInformationListItems.js
+++ src/components/app/BriefInformationListItems.js
@@ -8,7 +8,7 @@ export const BriefInformationListText = ({ svg, alt, infoText, width = 24, heigh
 );
 
 export const BriefInformationListLink = ({ href, svg, alt, callToAction, width = 24, height = 24 }) => (
-  <a className="information-list-link" target="_blank" href={href}>
+  <a key={callToAction} className="information-list-link" target="_blank" href={href}>
     <div className="information-list-link-content flex flex-row space-x-2">
       <Image src={svg} width={width} height={height} alt={alt} />
       <p className="text-md font-semibold text-blue-600 lg:text-blue-800">{callToAction}</p>
