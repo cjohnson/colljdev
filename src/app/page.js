8b0c52eaed85ec52bc2836bd35fda27fcf560663
cjohnson <collin.g.johnson@gmail.com>
2023-05-20 20:55:05 -0400

    Add relevant class names for identification

--- src/app/page.js
+++ src/app/page.js
@@ -12,9 +12,9 @@ export default function Home() {
 
 function HomeHero() {
   return (
-    <section className="bg-white dark:bg-gray-900">
-      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-        <div className="mr-auto place-self-center lg:col-span-7 md:pl-6">
+    <section className="home-hero bg-white dark:bg-gray-900">
+      <div className="home-hero-content grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="home-hero-main-content mr-auto place-self-center lg:col-span-7 md:pl-6">
           <HomeHeroText />
           <HomeHeroBriefInformationList />
         </div>
@@ -27,11 +27,11 @@ function HomeHero() {
 function HomeHeroText() {
   return (
     <>
-      <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+      <h1 className="home-hero-title max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
         Pursuing a career in Software Engineering.
       </h1>
-      <p className="max-w-2xl mb-6 font-medium text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-        Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan. 
+      <p className="home-hero-subtitle max-w-2xl mb-6 font-medium text-gray-500 lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400">
+        Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan College of Engineering.
         He is working an internship at AdvantageCS.
       </p>
     </>
@@ -40,7 +40,7 @@ function HomeHeroText() {
 
 function HomeHeroBriefInformationList() {
   return (
-    <div className="flex flex-col space-y-3 md:space-y-5 items-start">
+    <div className="home-hero-brief-information flex flex-col space-y-3 md:space-y-5 items-start">
       <InformationListItem
         svg="icons/location-sharp.svg"
         alt="Location icon"
--- src/components/app/HomeHeadingBriefInformation.js
+++ src/components/app/HomeHeadingBriefInformation.js
@@ -1,15 +1,15 @@
 import Image from "next/image";
 
 export const InformationListItem = ({ svg, alt, infoText, width = 24, height = 24 }) => (
-  <div className="flex flex-row space-x-2">
+  <div className="information-list-text flex flex-row space-x-2">
     <Image src={svg} width={width} height={height} alt={alt} />
     <p className="text-md">{infoText}</p>
   </div>
 );
 
 export const InformationListItemLink = ({ href, svg, alt, callToAction, width = 24, height = 24 }) => (
-  <a target="_blank" href={href}>
-    <div className="flex flex-row space-x-2">
+  <a className="information-list-link" target="_blank" href={href}>
+    <div className="information-list-link-content flex flex-row space-x-2">
       <Image src={svg} width={width} height={height} alt={alt} />
       <p className="text-md font-semibold">{callToAction}</p>
     </div>
