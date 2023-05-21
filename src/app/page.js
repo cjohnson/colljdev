57459d9c274b2560dd180527e340587c10564ce9
cjohnson <collin.g.johnson@gmail.com>
2023-05-21 16:50:45 -0400

    Prefer root imports over file-relative

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
-import HomeHero from '../components/app/HomeHero';
-import Resume from '../components/app/Resume';
-import Footer from '../components/app/Footer';
+import HomeHero from '@/components/app/HomeHero';
+import Resume from '@/components/app/Resume';
+import Footer from '@/components/app/Footer';
 
 export default function Home() {
   return (
