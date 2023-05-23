243c7997b8fdfd8a8c181c90fd24cc32419d3a69
cjohnson <collin.g.johnson@gmail.com>
2023-05-22 20:29:19 -0400

    Completely parameterize HomeHero component

--- src/components/app/HomeHero/HomeHero.jsx
+++ src/components/app/HomeHero/HomeHero.jsx
@@ -3,26 +3,23 @@ import Image from 'next/image';
 
 import HomeHeroIconInfo from './HomeHeroIconInfo';
 
-import iconInfoData from '@/data/iconInfo.json';
-
 export default function HomeHero() {
-  const heroTitle = 'Pursuing a career in Software Engineering.';
-  const heroDescription = 'Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan College of Engineering. He is a Software Development Trainee intern at AdvantageCS.';
+  const data = require('@/data/app/homeHeroData');
 
   return (
     <section className="home-hero bg-white dark:bg-gray-900">
       <div className="home-hero-content grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="home-hero-main-content mr-auto place-self-start lg:col-span-7 md:pl-6">
           <h1 className="home-hero-title max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            {heroTitle}
+            {data.title}
           </h1>
           <p className="home-hero-description max-w-2xl mb-6 font-medium text-gray-500 lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400">
-            {heroDescription}
+            {data.description}
           </p>
-          <HomeHeroIconInfo data={iconInfoData} />
+          <HomeHeroIconInfo data={data.iconInfo} />
         </div>
         <div className="home-hero-image hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <Image src="/mugs/mug-homepage.jpg" width={400} height={600} alt="Collin Johnson" />
+          <Image src={data.image.src} width={400} height={600} alt={data.image.alt} />
         </div>
       </div>
     </section>
