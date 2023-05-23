8181771e5fab9855953e7dd9fd3ecfeeb8d35e8b
cjohnson <collin.g.johnson@gmail.com>
2023-05-23 18:58:21 -0400

    Move FooterIconLink into its own file

--- src/components/app/Footer/Footer.jsx
+++ src/components/app/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 
 import FooterLink from './FooterLink';
+import FooterIconLink from './FooterIconLink';
 
 export default function Footer() {
   return (
@@ -23,16 +23,5 @@ export default function Footer() {
         </ul>
       </div>
     </footer>
-  );
-}
-
-function FooterIconLink({ href, icon, callToAction, width = 12, height = 12 }) {
-  return (
-    <a target="_blank" href={href}>
-      <div className='flex flex-row space-x-1 place-items-center'>
-        <FontAwesomeIcon icon={icon} width={width} height={height} />
-        <p>{callToAction}</p>
-      </div>
-    </a>
   );
 }
