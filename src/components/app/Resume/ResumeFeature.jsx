cd9833e6f7bde6355ca89b4f7e24dfd6a52a50ef
cjohnson <collin.g.johnson@gmail.com>
2023-05-22 20:50:50 -0400

    Move extras content to new line with new styling

--- src/components/app/Resume/ResumeFeature.jsx
+++ src/components/app/Resume/ResumeFeature.jsx
@@ -5,9 +5,10 @@ export default function ResumeFeature({ position, relation, organization, timePe
     <li key={position} className="resume-feature mb-10 ml-4">
       <div className="vertical-timeline-point absolute w-3 lg:w-4 h-3 lg:h-4 bg-gray-300 rounded-full mt-1.5 lg:mt-2 -left-[6.57px] lg:-left-[8.88px] border border-white dark:border-gray-900 dark:bg-gray-700"></div>
       <h2 className="resume-feature-title sm:text-2xl lg:text-2xl tracking-tight font-regular text-gray-900 dark:text-white">
-        <span className="font-bold">{position}</span> {relation} <span className="font-bold">{organization}</span> {extras}
+        <span className="font-bold">{position}</span> {relation} <span className="font-bold">{organization}</span>
       </h2>
-      <h3 className="resume-feature-time-period mb-2 tracking-tight font-light text-gray-500 dark:text-white">{timePeriod}</h3>
+      <h3 className="resume-feature-extras tracking-normal font-semibold text-gray-500 dark:text-white">{extras}</h3>
+      <h4 className="resume-feature-time-period mb-2 tracking-tight font-light text-gray-500 dark:text-white">{timePeriod}</h4>
       {description !== null && <p className="resume-feature-description font-normal text-gray-500 dark:text-white">{description}</p>}
     </li>
   );
