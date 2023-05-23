a4f4048ed0dab3ad959f92419be191a6e2b89055
cjohnson <collin.g.johnson@gmail.com>
2023-05-22 20:12:24 -0400

    Add parameterization for HomeHero title and description components, rename 'subtitle' class to 'description' class

--- src/components/app/HomeHero/HomeHero.jsx
+++ src/components/app/HomeHero/HomeHero.jsx
@@ -6,16 +6,18 @@ import HomeHeroIconInfo from './HomeHeroIconInfo';
 import iconInfoData from '@/data/iconInfo.json';
 
 export default function HomeHero() {
+  const heroTitle = 'Pursuing a career in Software Engineering.';
+  const heroDescription = 'Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan College of Engineering. He is a Software Development Trainee intern at AdvantageCS.';
+
   return (
     <section className="home-hero bg-white dark:bg-gray-900">
       <div className="home-hero-content grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="home-hero-main-content mr-auto place-self-start lg:col-span-7 md:pl-6">
           <h1 className="home-hero-title max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
-            Pursuing a career in Software Engineering.
+            {heroTitle}
           </h1>
-          <p className="home-hero-subtitle max-w-2xl mb-6 font-medium text-gray-500 lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400">
-            Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan College of Engineering.
-            He is a Software Development Trainee intern at AdvantageCS.
+          <p className="home-hero-description max-w-2xl mb-6 font-medium text-gray-500 lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400">
+            {heroDescription}
           </p>
           <HomeHeroIconInfo data={iconInfoData} />
         </div>
