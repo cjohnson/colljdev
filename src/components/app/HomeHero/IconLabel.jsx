790a2df4810ab562e26904d4426f2c5e067cfb2d
cjohnson <collin.g.johnson@gmail.com>
2023-05-22 23:03:15 -0400

    Remove old Image declarations in IconLabel and IconLink

--- src/components/app/HomeHero/IconLabel.jsx
+++ src/components/app/HomeHero/IconLabel.jsx
@@ -6,7 +6,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function IconLabel({ svg, alt, icon, infoText, width = 24, height = 24 }) {
   return (
     <div className="information-list-text flex flex-row space-x-2">
-      <Image src={svg} width={width} height={height} alt={alt} />
       <FontAwesomeIcon icon={icon} width={width} height={height} />
       <p className="text-md">{infoText}</p>
     </div>
--- src/components/app/HomeHero/IconLink.jsx
+++ src/components/app/HomeHero/IconLink.jsx
@@ -7,7 +7,6 @@ export default function IconLink({ href, svg, icon, alt, callToAction, width = 2
   return (
     <a className="information-list-link" target="_blank" href={href}>
       <div className="information-list-link-content flex flex-row space-x-2">
-        <Image src={svg} width={width} height={height} alt={alt} />
         <FontAwesomeIcon icon={icon} width={width} height={height} />
         <p className="text-md font-semibold text-blue-600 lg:text-blue-800">{callToAction}</p>
       </div>
