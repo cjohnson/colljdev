1f1a18037ee3ade66a072c00aaba460385d54570
cjohnson <collin.g.johnson@gmail.com>
2023-05-22 22:31:57 -0400

    Convert homeHeroData to data provider function export

---
+++ src/data/app/homeHeroData.js
@@ -0,0 +1,42 @@
+export default function getData() {
+  return {
+    title: 'Pursuing a career in Software Engineering.',
+    description: 'Collin Johnson is pursuing a degree in Computer Science and Engineering (CSE) at the University of Michigan College of Engineering. He is a Software Development Trainee intern at AdvantageCS.',
+    image: {
+      src: '/mugs/mug-homepage.jpg',
+      alt: 'Collin Johnson'
+    },
+    iconInfo: {
+      labels: [
+        {
+          key: 0,
+          svg: 'icons/location-sharp.svg',
+          alt: 'Location icon',
+          content: 'Ann Arbor, Michigan'
+        },
+        {
+          key: 1,
+          svg: 'icons/school-sharp.svg',
+          alt: 'School icon',
+          content: 'University of Michigan'
+        }
+      ],
+      links: [
+        {
+          key: 0,
+          href: 'https://linkedin.com/in/collj/',
+          svg: 'socials/logo-linkedin.svg',
+          alt: 'LinkedIn logo',
+          callToAction: 'Connect on LinkedIn'
+        },
+        {
+          key: 1,
+          href: 'https://github.com/cjohnson/',
+          svg: 'socials/logo-github.svg',
+          alt: 'Github logo',
+          callToAction: 'Follow on Github'
+        }
+      ]
+    }
+  };
+}
