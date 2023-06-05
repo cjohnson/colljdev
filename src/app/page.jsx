d39dece39997c3285c47c70334dcb6190f532b45
cjohnson <collin.g.johnson@gmail.com>
2023-06-04 20:58:55 -0400

    Add manual tailwind dark mode switching

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,12 +4,14 @@ import HomeHero from '@/components/app/HomeHero';
 import Resume from '@/components/app/Resume';
 import Footer from '@/components/app/Footer';
 
+const theme = 'dark';
+
 export default function Home() {
   return (
-    <>
+    <div className={theme === 'dark' ? 'dark' : ''}>
       <HomeHero />
       <Resume />
       <Footer />
-    </>
+    </div>
   )
 }
